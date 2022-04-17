da7755afbabff34b583adc9ae568cb1c9c92f917
samie-ya <samrasolomon40@gmail.com>
2022-04-17 17:59:44 +0000

    Modified __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-"""This script will contain stuff to be imported"""
+"""
+initialize the web application
+"""
+
 from flask import Blueprint
 
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 
 from api.v1.views.index import *
 from api.v1.views.states import *
@@ -10,4 +15,5 @@ from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
-from api.v1.views.reviews import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *

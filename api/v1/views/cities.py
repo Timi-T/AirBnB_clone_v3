8ddef6a3bf2e37391bbea74c6b31b190f619ee19
samie-ya <samrasolomon40@gmail.com>
2022-04-17 21:48:39 +0000

    Removed strict slashes

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -10,8 +10,7 @@ from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
 
 
-@app_views.route('/states/<state_id>/cities',  strict_slashes=False,
-                 methods=['GET'])
+@app_views.route('/states/<state_id>/cities', methods=['GET'])
 def cities_get(state_id):
     """
     Get method for all cities linked to a state
@@ -26,8 +25,7 @@ def cities_get(state_id):
     abort(404)
 
 
-@app_views.route('/cities/<city_id>', strict_slashes=False,
-                 methods=['GET'])
+@app_views.route('/cities/<city_id>', methods=['GET'])
 def city_get(city_id):
     """
     Get a city using its id
@@ -37,8 +35,7 @@ def city_get(city_id):
         return jsonify(city.to_dict())
     abort(404)
 
-@app_views.route('/cities/<city_id>', strict_slashes=False,
-                 methods=['PUT'])
+@app_views.route('/cities/<city_id>', methods=['PUT'])
 def state_update(city_id):
     """
     Update a state object in the database
@@ -60,8 +57,7 @@ def state_update(city_id):
     abort(404)
 
 
-@app_views.route('/cities/<city_id>', strict_slashes=False,
-                 methods=['DELETE'])
+@app_views.route('/cities/<city_id>', methods=['DELETE'])
 def city_delete(city_id):
     """
     Delete a city object
@@ -74,8 +70,7 @@ def city_delete(city_id):
     else:
         abort(404)
 
-@app_views.route('/states/<state_id>/cities', strict_slashes=False,
-                 methods=['POST'])
+@app_views.route('/states/<state_id>/cities', methods=['POST'])
 def city_create(state_id):
     """
     Create a new city object

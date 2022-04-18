aeda402da30d40c3bd947528889ee1bc5ab31aba
samie-ya <samrasolomon40@gmail.com>
2022-04-18 21:05:24 +0000

    removed str from user

--- models/user.py
+++ models/user.py
@@ -36,4 +36,4 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
-        self.password = str(get_hash(self.password))
+        self.password = get_hash(self.password)
